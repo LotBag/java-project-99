bbdd62cd9ad8a5244f1985ee12e08a5c53bd64ba
LotBag <rusianovoleg@list.ru>
2024-07-31 16:33:51 +0700

    add Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,23 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
+	application
+	id("jacoco")
+	id("checkstyle")
+	id("io.freefair.lombok") version "8.4"
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("io.sentry.jvm.gradle") version "4.3.1"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
@@ -18,7 +26,6 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -33,8 +40,21 @@ dependencies {
 	implementation("org.springframework.security.oauth:spring-security-oauth2:2.5.2.RELEASE")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
+
+	implementation("jakarta.validation:jakarta.validation-api:3.1.0-M1")
+	implementation("net.datafaker:datafaker:2.1.0")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("com.h2database:h2:2.2.224")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.instancio:instancio-junit:4.1.0")
+	testImplementation(platform("org.junit:junit-bom:5.10.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
 }
 
 tasks.withType<Test> {
