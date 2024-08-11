07240b2250d3acbe0c8e1a31bef1289028d99709
LotBag <rusianovoleg@list.ru>
2024-08-11 15:39:00 +0700

    add Mapper

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class WelcomeController {
+@RequestMapping(path = "/welcome")
+public final class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping
     public String welcome() {
         return "Welcome to Spring";
     }
