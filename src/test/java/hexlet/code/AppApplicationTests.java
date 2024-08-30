1f8325c8a295b5756223b67959fc79a952ec35b0
LotBag <rusianovoleg@list.ru>
2024-08-31 02:02:03 +0700

    linter fix

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -32,84 +32,84 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 
 class AppApplicationTests {
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private ModelGenerator modelGenerator;
+    @Autowired
+    private ModelGenerator modelGenerator;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	private JwtRequestPostProcessor token;
+    private JwtRequestPostProcessor token;
 
-	private User testUser;
+    private User testUser;
 
-	@BeforeEach
-	public void setUp() {
-		token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
-		testUser = Instancio.of(modelGenerator.getUserModel()).create();
-		testUser.setPasswordDigest(passwordEncoder.encode("12345678"));
-		System.out.println("testUser before save -> " + testUser);
-		userRepository.save(testUser);
-		System.out.println("testUser after save -> " + testUser);
-	}
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
+        testUser = Instancio.of(modelGenerator.getUserModel()).create();
+        testUser.setPasswordDigest(passwordEncoder.encode("12345678"));
+        System.out.println("testUser before save -> " + testUser);
+        userRepository.save(testUser);
+        System.out.println("testUser after save -> " + testUser);
+    }
 
-	@Test
-	void testIndex() throws Exception {
-		mockMvc.perform(get("/api/users").with(jwt())).andExpect(status().isOk());
-	}
+    @Test
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/api/users").with(jwt())).andExpect(status().isOk());
+    }
 
-	@Test
-	void testCreate() throws Exception {
-		User data = Instancio.of(modelGenerator.getUserModel()).create();
+    @Test
+    void testCreate() throws Exception {
+        User data = Instancio.of(modelGenerator.getUserModel()).create();
 
-		MockHttpServletRequestBuilder request = post("/api/users")
-				.with(token)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(data));
-		mockMvc.perform(request)
-				.andExpect(status().isCreated());
+        MockHttpServletRequestBuilder request = post("/api/users")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
 
-		User user = userRepository.findByEmail(data.getEmail()).get();
+        User user = userRepository.findByEmail(data.getEmail()).get();
 
-		assertNotNull(user);
-		assertThat(user.getFirstName()).isEqualTo(data.getFirstName());
-		assertThat(user.getLastName()).isEqualTo(data.getLastName());
-	}
+        assertNotNull(user);
+        assertThat(user.getFirstName()).isEqualTo(data.getFirstName());
+        assertThat(user.getLastName()).isEqualTo(data.getLastName());
+    }
 
-	@Test
-	void testUpdate() throws Exception {
-		HashMap<String, String> data = new HashMap<>();
-		data.put("firstName", "Mike");
+    @Test
+    void testUpdate() throws Exception {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("firstName", "Mike");
 
-		HashMap<String, String> logoPass = new HashMap<>();
-		logoPass.put("username", testUser.getEmail());
-		logoPass.put("password", "12345678");
+        HashMap<String, String> logoPass = new HashMap<>();
+        logoPass.put("username", testUser.getEmail());
+        logoPass.put("password", "12345678");
 
-		MockHttpServletRequestBuilder requestToken = post("/api/login")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(logoPass));
+        MockHttpServletRequestBuilder requestToken = post("/api/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(logoPass));
 
-		String testToken = mockMvc.perform(requestToken).andReturn().getResponse().getContentAsString();
+        String testToken = mockMvc.perform(requestToken).andReturn().getResponse().getContentAsString();
 
-		MockHttpServletRequestBuilder request = put("/api/users/" + testUser.getId())
+        MockHttpServletRequestBuilder request = put("/api/users/" + testUser.getId())
 //                .with(token)
-				.header("Authorization", "Bearer " + testToken)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(data));
+                .header("Authorization", "Bearer " + testToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
 
 
-		mockMvc.perform(request)
-				.andExpect(status().isOk());
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
 
-		User user = userRepository.findById(testUser.getId()).get();
-		assertThat(user.getFirstName()).isEqualTo("Mike");
-	}
+        User user = userRepository.findById(testUser.getId()).get();
+        assertThat(user.getFirstName()).isEqualTo("Mike");
+    }
 }
