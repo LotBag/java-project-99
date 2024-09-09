58df3b08d248fe7851d7bd05720bfaa44be81a9b
LotBag <rusianovoleg@list.ru>
2024-09-09 19:55:20 +0700

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,28 +32,18 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-configuration-processor")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.security.oauth:spring-security-oauth2:2.5.2.RELEASE")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 	implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
-
-	implementation("jakarta.validation:jakarta.validation-api:3.1.0-M1")
 	implementation("net.datafaker:datafaker:2.1.0")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("com.h2database:h2:2.2.224")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.instancio:instancio-junit:4.1.0")
@@ -67,9 +57,6 @@ tasks.withType<Test> {
 }
 
 sentry {
-	// Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
-	// This enables source context, allowing you to see your source
-	// code as part of your stack traces in Sentry.
 	includeSourceContext = true
 
 	org = "06f2616b374b"
@@ -79,7 +66,6 @@ sentry {
 
 tasks.test {
 	useJUnitPlatform()
-	// https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
 	testLogging {
 		exceptionFormat = TestExceptionFormat.FULL
 		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
